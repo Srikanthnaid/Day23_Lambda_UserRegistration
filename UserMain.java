fcad0f5d4bd99d9f9f22972659f4f13688dcb713
Srikanthnaid <ksru044gmail.com>
2022-09-23 10:02:36 +0530

    password eneter folloeing rule1

--- UserMain.java
+++ UserMain.java
@@ -1,8 +1,8 @@
 package UserRegistrationLambda;
 
 public class UserMain {
-
 	public static void main(String[] args) {
+
 		/*
 		 * Creating object of userDetails class
 		 */
@@ -16,7 +16,22 @@ public class UserMain {
 		 * calling LastName method to ensure valid Last name
 		 */
 		user.lastName();
-
+		/**
+		 * calling email method to ensure valid email
+		 */
+		user.email();
+		/**
+		 * calling moblieNumber method to ensure valid mobile Number
+		 */
+		user.moblieNumber();
+		/**
+		 * calling password method to ensure valid password of minimum 8 characters
+		 */
+		user.password();
+		/**
+		 * calling password method to ensure valid password of minimum 8 characters and
+		 * have at least 1 uppercase
+		 */
+		user.password();
 	}
-
 }
